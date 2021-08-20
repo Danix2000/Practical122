02b8cbf09d691824dff74c6d7d455710b78158f1
Danix2000 <79327683+Danix2000@users.noreply.github.com>
2021-08-20 09:00:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Practical122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is C# and this is a practical example!!!");
+        }
+    }
+}
